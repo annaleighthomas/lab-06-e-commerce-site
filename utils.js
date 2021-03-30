9c5262ea0abac46d65a0fe50e6c5ee07f7528e20
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-03-30 18:13:33 -0400

    createPlants func

--- utils.js
+++ utils.js
@@ -1,14 +1,41 @@
 export function createPlants(plant){
     const li = document.createElement(li);
 
-    
+    li.classList.add('plant');
 
+    const pId = document.createElement('p');
 
+    pName.classList.add('id');
 
+    const pName = document.createElement('p');
 
+    pName.classList.add('name');
+    pName.textContent = plant.name;
 
+    const plantImage = document.createElement('img');
 
+    plantImage.src = plant.image;
 
+    const pDescription = document.createElement('p');
 
+    pDescription.classList.add('description');
+    pDescription.textContent = plant.description;
 
+    const pCategory = document.createElement('p');
+
+    pCategory.classList.add('category');
+    pCategory.textContent = plant.category;
+
+    const pSun = document.createElement('p');
+
+    pSun.classList.add('sun');
+    pSun.textContent = plant.sun;
+
+    const pPrice = document.createElement('p');
+
+    pPrice.classList.add('price');
+    pPrice.textContent = plant.price.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }); 
 }
